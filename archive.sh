e501797e173156ee56e3d2d7040ab977c7e41829
Adam Talsma <github@talsma.ca>
2017-02-08 11:15:10 -0800

    make curl fail on error

--- archive.sh
+++ archive.sh
@@ -3,7 +3,7 @@
 function archive {
   pushd .
   cd $1
-  curl "https://esi.tech.ccp.is/$1/swagger.json" | python3.5 -m json.tool --sort-keys > swagger.json && git add swagger.json && git commit --author "ESI archive bot <archive@pizza.moe>" -m "checked in new $1 definition"
+  curl -f "https://esi.tech.ccp.is/$1/swagger.json" | python3.5 -m json.tool --sort-keys > swagger.json && git add swagger.json && git commit --author "ESI archive bot <archive@pizza.moe>" -m "checked in new $1 definition"
   popd
 }
 
