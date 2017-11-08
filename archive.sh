6730725f4dba940ad0c64997b89dbd37c51ff231
Andi Miller <andi@andimiller.net>
2017-11-08 14:09:09 +0000

    upgrade to be Last-Modified aware

--- archive.sh
+++ archive.sh
@@ -3,7 +3,20 @@
 function archive {
   pushd .
   cd $1
-  curl -f "https://esi.tech.ccp.is/$1/swagger.json" | python3.5 -m json.tool --sort-keys > swagger.json && git add swagger.json && git commit --author "ESI archive bot <archive@pizza.moe>" -m "checked in new $1 definition"
+  LASTDATE=$(date -d "`grep "Last-Modified:" headers.txt | sed 's/^Last-Modified: //g'`" +%s)
+  curl -D headers.txt -f "https://esi.tech.ccp.is/$1/swagger.json" | python3.5 -m json.tool --sort-keys > swagger.json
+  NEWDATE=$(date -d "`grep "Last-Modified:" headers.txt | sed 's/^Last-Modified: //g'`" +%s)
+
+  echo "$NEWDATE vs $LASTDATE"
+  if [ $NEWDATE -gt $LASTDATE ];
+  then
+    echo "new content found for $1"
+    git add swagger.json headers.txt
+    git commit --author "ESI Archiver <archive@pizza.moe>" -m "new $1 swagger definition at `grep "Last-Modified:" headers.txt | sed 's/^Last-Modified: //g'`"
+  else
+    echo "no new content found for $1"
+    git checkout -- headers.txt swagger.json
+  fi
   popd
 }
 
