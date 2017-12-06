3dee02ecbed258bdc8010a2b4558ba8fba2c7778
Andi Miller <andi@andimiller.net>
2017-12-06 11:35:53 +0000

    if curl failed just check back outta git and do nothing

--- archive.sh
+++ archive.sh
@@ -6,25 +6,30 @@ function archive {
   LASTDATE=$(date -d "`grep "Last-Modified:" headers.txt | sed 's/^Last-Modified: //g'`" +%s)
   curl -D headers.txt -f "https://esi.tech.ccp.is/$1/swagger.json" | python3.5 -m json.tool --sort-keys > swagger.json
   NEWDATE=$(date -d "`grep "Last-Modified:" headers.txt | sed 's/^Last-Modified: //g'`" +%s)
+  HEADERCOUNT=$(wc -l headers.txt | cut -d' ' -f1)
 
-  echo "$NEWDATE vs $LASTDATE"
-  if [ $NEWDATE -gt $LASTDATE ];
-  then
-    echo "new content found for $1"
-    git status --porcelain | grep "swagger.json"
-    NEWSWAGGER=$?
-    if [ $NEWSWAGGER -eq 0 ]; then
-      git add swagger.json headers.txt
-      git commit --author "ESI Archiver <archive@pizza.moe>" -m "$1 at `grep "Last-Modified:" headers.txt | sed 's/^Last-Modified: //g'`"
+  if [ $HEADERCOUNT -gt 0 ]; then
+    echo "$NEWDATE vs $LASTDATE"
+    if [ $NEWDATE -gt $LASTDATE ];
+    then
+      echo "new content found for $1"
+      git status --porcelain | grep "swagger.json"
+      NEWSWAGGER=$?
+      if [ $NEWSWAGGER -eq 0 ]; then
+        git add swagger.json headers.txt
+        git commit --author "ESI Archiver <archive@pizza.moe>" -m "$1 at `grep "Last-Modified:" headers.txt | sed 's/^Last-Modified: //g'`"
+      else
+        echo "only the headers changed"
+        git checkout -- headers.txt
+      fi
     else
-      echo "only the headers changed"
-      git checkout -- headers.txt
+      echo "no new content found for $1"
+      git checkout -- headers.txt swagger.json
     fi
+    popd
   else
-    echo "no new content found for $1"
     git checkout -- headers.txt swagger.json
   fi
-  popd
 }
 
 archive latest
